c4a1af7015da2aa831d401a5cc7762476526fbf0
Jahid-Hasan-Jony <jonyhossain5588@gmail.com>
2025-04-18 16:26:46 +0600

    Initial commit

--- index.php
+++ index.php
@@ -1,34 +1,34 @@
 <?php 
 
-// $name = "Ali hossain";
-// $age = 23;
-// echo "My name is $name and I am $age years old.";
+$name = "Ali hossain";
+$age = 23;
+echo "My name is $name and I am $age years old.";
 
 
 
-// $int = 50;
-// $float = 50.50;
-// $bool = true;
-// $null = null;
-// echo "Integer: $int, Float: $float, Boolean: $bool, Null: $null\n";
+$int = 50;
+$float = 50.50;
+$bool = true;
+$null = null;
+echo "Integer: $int, Float: $float, Boolean: $bool, Null: $null\n";
 
 
 
-// $array = array ('apple', 'banana', 'orange');
-// // $array = array('apple', 'banana', 'orange');
-// print_r($array);
+$array = array ('apple', 'banana', 'orange');
+// $array = array('apple', 'banana', 'orange');
+print_r($array);
 
 
-// define( 'PT', '3.14');
-// const DB_NAME = 'test';
+define( 'PT', '3.14');
+const DB_NAME = 'test';
 
 
-// $name = "Ali Hossain";
-// $age = 23;
-// $float = 30.3;
-// $bool = true;
-// $null = null;
-// $array = array('apple', 'banana', 12);
+$name = "Ali Hossain";
+$age = 23;
+$float = 30.3;
+$bool = true;
+$null = null;
+$array = array('apple', 'banana', 12);
 
 
 var_dump($name);
