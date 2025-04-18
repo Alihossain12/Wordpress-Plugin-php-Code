168751b684cb9e82fd0dca9c6f3d39d941aa723e
Jahid-Hasan-Jony <jonyhossain5588@gmail.com>
2025-04-18 16:00:17 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php 
+
+// $name = "Ali hossain";
+// $age = 23;
+// echo "My name is $name and I am $age years old.";
+
+
+
+// $int = 50;
+// $float = 50.50;
+// $bool = true;
+// $null = null;
+// echo "Integer: $int, Float: $float, Boolean: $bool, Null: $null\n";
+
+
+
+// $array = array ('apple', 'banana', 'orange');
+// // $array = array('apple', 'banana', 'orange');
+// print_r($array);
+
+
+// define( 'PT', '3.14');
+// const DB_NAME = 'test';
+
+
+// $name = "Ali Hossain";
+// $age = 23;
+// $float = 30.3;
+// $bool = true;
+// $null = null;
+// $array = array('apple', 'banana', 12);
+
+
+// var_dump($name);
+// var_dump($age);
+// var_dump($float);
+// var_dump($bool);
+// var_dump($null);
+// var_dump($array);
+
+
+?>
