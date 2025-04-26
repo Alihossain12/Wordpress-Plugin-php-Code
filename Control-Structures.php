331c21dad30528a52ae152e2faa7246f9ff5f135
Alihossain12 <mdalihossain32246886@gmail.com>
2025-04-26 12:32:40 +0600

    Control-Structures

---
+++ Control-Structures.php
@@ -0,0 +1,17 @@
+
+<?php
+
+// Control Structures (if, else, switch)
+
+
+ $name = "Ali Hossain";
+ $age = 20;
+
+ if ($age >= 18) {
+     echo "$name is an adult.";
+ } else {
+     echo "$name is not an adult.";
+ }
+
+
+?>
--- Operators-and-Expressions.php
+++ Operators-and-Expressions.php
@@ -3,74 +3,74 @@
 
 // Arithmetic Operators:// +, -, *, /, %
 
-// $a = 10;
-// $b = 3;
+$a = 10;
+$b = 3;
 
-// echo $a + $b; // Addition
-// echo "<br>";
-// echo $a - $b; // Subtraction
-// echo "<br>";
-// echo $a * $b; // Multiplication
-// echo "<br>";
-// echo $a / $b; // Division
-// echo "<br>";
-// echo $a % $b; // Modulus
-// echo "<br>";
+echo $a + $b; // Addition
+echo "<br>";
+echo $a - $b; // Subtraction
+echo "<br>";
+echo $a * $b; // Multiplication
+echo "<br>";
+echo $a / $b; // Division
+echo "<br>";
+echo $a % $b; // Modulus
+echo "<br>";
 
 
 
 //Assignment:// =, +=, -=, *=, /=, %=
 
-// $a = 100;
-// $b = 20;
+$a = 100;
+$b = 20;
 
-// $a += $b; // $a = $a + $b
-// echo $a;
-// echo "<br>";
+$a += $b; // $a = $a + $b
+echo $a;
+echo "<br>";
 
-// $a -= $b; // $a = $a - $b
-// echo $a; 
-// echo "<br>";
+$a -= $b; // $a = $a - $b
+echo $a; 
+echo "<br>";
 
-// $a *= $b; // $a = $a * $b
-// echo "<br>";
+$a *= $b; // $a = $a * $b
+echo "<br>";
 
-// $a /= $b;  // $a = $a / $b
-// echo $a;
-// echo "<br>";
+$a /= $b;  // $a = $a / $b
+echo $a;
+echo "<br>";
 
-// $a %= $b; echo $a; // $a = $a % $b
-//  echo "<br>";
+$a %= $b; echo $a; // $a = $a % $b
+ echo "<br>";
 
 
 //  Comparison:// ==, ===, !=, !==, >, <, >=, <=
 
-// $a = 10;
-// $b = "10";
+$a = 10;
+$b = "10";
 
-// var_dump($a == $b); // true 
-// echo "<br>";
+var_dump($a == $b); // true 
+echo "<br>";
 
-// var_dump($a === $b); // false
-// echo "<br>";
+var_dump($a === $b); // false
+echo "<br>";
 
-// var_dump($a != $b); // false
-// echo "<br>";
+var_dump($a != $b); // false
+echo "<br>";
 
-// var_dump($a !== $b); // true
-// echo "<br>";
+var_dump($a !== $b); // true
+echo "<br>";
 
-// var_dump($a > $b); // false
-// echo "<br>";
+var_dump($a > $b); // false
+echo "<br>";
 
-// var_dump($a < $b); // false
-// echo "<br>";
+var_dump($a < $b); // false
+echo "<br>";
 
-// var_dump($a >= $b); // true
-// echo "<br>";
+var_dump($a >= $b); // true
+echo "<br>";
 
-// var_dump($a <= $b); // true
-// echo "<br>";
+var_dump($a <= $b); // true
+echo "<br>";
 
 
 
