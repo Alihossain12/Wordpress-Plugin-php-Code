a66a853a34ee27725de5a6d2d1f725320fddb140
Alihossain12 <mdalihossain32246886@gmail.com>
2025-04-26 14:23:05 +0600

    Loop

--- Loop.php
+++ Loop.php
@@ -5,52 +5,52 @@
 // 1. for Loop
 
 
-// for ($i = 1; $i <= 5; $i++) {
-//     echo "Iteration: $i";
-//     echo "<br>";
-// }
+for ($i = 1; $i <= 5; $i++) {
+    echo "Iteration: $i";
+    echo "<br>";
+}
 
 
-//  2. while Loop-------------------
+ // 2. while Loop-------------------
 
-// $i = 1;
+$i = 1;
 
-// while ($i <= 50) {
-//     echo "Iteration: $i";
-//     echo "<br>";
-//     $i++;
-// }
+while ($i <= 50) {
+    echo "Iteration: $i";
+    echo "<br>";
+    $i++;
+}
 
 //  3. do...while Loop-------------------
 
 
-// $i = 1;
+$i = 1;
 
-// do {
-//     echo "Iteration: $i";
-//     echo "<br>";
-//     $i++;
-// } while ($i <= 5);
+do {
+    echo "Iteration: $i";
+    echo "<br>";
+    $i++;
+} while ($i <= 5);
 
 
 // 4. foreach Loop-------------------
 
-//  $foreachArray = ["Ali", "Hossain", "Sakib", "Riyad", "Shakib"];
+ $foreachArray = ["Ali", "Hossain", "Sakib", "Riyad", "Shakib"];
 
-//  foreach ($foreachArray as $value) {
-//      echo "Value: $value";
-//      echo "<br>";
-//  }
+ foreach ($foreachArray as $value) {
+     echo "Value: $value";
+     echo "<br>";
+ }
 
 
  //Example 1 (Indexed):-------------------
 
-// $indexedArray = ["Ali", "Hossain", "Sakib", "Riyad", "Shakib"];
+$indexedArray = ["Ali", "Hossain", "Sakib", "Riyad", "Shakib"];
 
-// foreach ($indexedArray as $value) {
-//     echo "Value: $value";
-//     echo "<br>";
-// }
+foreach ($indexedArray as $value) {
+    echo "Value: $value";
+    echo "<br>";
+}
 
 
 // Example 2 (Associative):-------------------
