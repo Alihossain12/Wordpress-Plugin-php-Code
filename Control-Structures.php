5784ea849b1aa4a5ccee1e9a8ef3fe7059b0784f
Alihossain12 <mdalihossain32246886@gmail.com>
2025-04-26 12:45:15 +0600

    Control-Structures

--- Control-Structures.php
+++ Control-Structures.php
@@ -4,6 +4,9 @@
 // Control Structures (if, else, switch)
 
 
+// 1. If Statement -------------------------
+
+
  $name = "Ali Hossain";
  $age = 20;
 
@@ -13,5 +16,51 @@
      echo "$name is not an adult.";
  }
 
+
+ //  2. if...else Statement ----------------------
+
+    $age = 23;
+
+    if ($age < 30) {
+        echo "You are an adult.";
+    } else {
+        echo "You are a senior citizen."; 
+
+    }
+
+
+    //3. if...elseif...else Statement
+
+    $age = 20;
+
+    if ($age < 18) {
+        echo "You are a minor.";
+    } elseif ($age >= 18 && $age < 30) {
+        echo "You are an adult.";
+    } else {
+        echo "You are a senior citizen."; 
+    }
+
+
+
+
+    // 4. Switch Statement ----------------
+
+
+    $day = "Monday";
+
+    switch ($day) {
+        case "Monday":
+            echo "Today is Monday.";
+            break;
+        case "Tuesday":
+            echo "Today is Tuesday.";
+            break;
+        case "Wednesday":
+            echo "Today is Wednesday.";
+            break;
+        default:
+            echo "Invalid day.";
+    }
 
 ?>
