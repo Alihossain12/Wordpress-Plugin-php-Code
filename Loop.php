09d9a9e63bc06c6e9715cc4044bdfcfe57958da4
Alihossain12 <mdalihossain32246886@gmail.com>
2025-05-01 15:14:48 +0600

    Loop

--- Loop.php
+++ Loop.php
@@ -111,33 +111,60 @@
 
 
 
-$products = array(
-    'name' => ['Product 1', 'Product 2', 'Product 3'],
-    'price' => [100, 200, 300],
-    'quantity' => [10, 20, 30],
-    'category' => ['Electronics', 'Clothing', 'Books'],
-    'brand' => ['Brand A', 'Brand B', 'Brand C'],
-    'id' => [1, 2, 3],
-    'description' => ['Description of Product 1', 'Description of Product 2', 'Description of Product 3'],
+// $products = array(
+//     'name' => ['Product 1', 'Product 2', 'Product 3'],
+//     'price' => [100, 200, 300],
+//     'quantity' => [10, 20, 30],
+//     'category' => ['Electronics', 'Clothing', 'Books'],
+//     'brand' => ['Brand A', 'Brand B', 'Brand C'],
+//     'id' => [1, 2, 3],
+//     'description' => ['Description of Product 1', 'Description of Product 2', 'Description of Product 3'],
+// );
+// foreach ($products['name'] as $index => $name) {
+//     echo "Product $index: ";
+//     echo "<br>";
+//     echo "Name: $name";
+//     echo "<br>";
+//     echo "Price: " . $products['price'][$index];
+//     echo "<br>";
+//     echo "Quantity: " . $products['quantity'][$index];
+//     echo "<br>";
+//     echo "Category: " . $products['category'][$index];
+//     echo "<br>";
+//     echo "Brand: " . $products['brand'][$index];
+//     echo "<br>";
+//     echo "ID: " . $products['id'][$index];
+//     echo "<br>";
+//     echo "Description: " . $products['description'][$index];
+//     echo "<br>";
+//     echo "<hr>";
+// }
+
+
+
+$students = array(
+  'name' => ['Ali Hossain', 'farhan', 'Naim'],
+  'roll' => [21, 22, 23],
+  'age' => [20, 21, 22],
+  'city' => ['Dhaka', 'Chittagong', 'Khulna'],
+  'country' => ['Bangladesh', 'India', 'Pakistan'],
+  'id' => [1, 2, 3],
+  'email' => ['ali@.com', 'farhan@.com', 'naim@.com'],
+  'phone' => ['01700000000', '01800000000', '01900000000'],
+  'postcode' => ['1212', '1213', '1214'],
 );
-foreach ($products['name'] as $index => $name) {
-    echo "Product $index: ";
-    echo "<br>";
-    echo "Name: $name";
-    echo "<br>";
-    echo "Price: " . $products['price'][$index];
-    echo "<br>";
-    echo "Quantity: " . $products['quantity'][$index];
-    echo "<br>";
-    echo "Category: " . $products['category'][$index];
-    echo "<br>";
-    echo "Brand: " . $products['brand'][$index];
-    echo "<br>";
-    echo "ID: " . $products['id'][$index];
-    echo "<br>";
-    echo "Description: " . $products['description'][$index];
-    echo "<br>";
-    echo "<hr>";
-}
+
+foreach ($students as $key => $value) {
+ echo $key  .  "\n";
+ if (is_array($value)) {
+      foreach ($value as $index => $item) {
+          echo "Index: $index, Value: $item\n";
+      }
+  } else {
+      echo "Value: $value\n";
+  }
+  echo "<br>";
+  echo "<hr>";
+};
 
 ?>
